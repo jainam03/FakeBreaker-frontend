b5359fe28164177045417657ef47df1993e9070b
Jainam <91124384+jainam03@users.noreply.github.com>
2025-03-15 21:17:36 +0530

    included dist directory

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: { 
+    'import.meta.env.VITE_APP_API_URL': JSON.stringify(process.env.VITE_APP_API_URL), 
+  },
   plugins: [react()],
 })
