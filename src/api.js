af80a9c4726a8ee4f91f44b2d77e755b6ac97ba9
Jainam <91124384+jainam03@users.noreply.github.com>
2025-03-15 20:57:10 +0530

    some more changes

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_APP_API_URL || "https://fakebreaker-flask-server.onrender.com";
 
 export const uploadAudio = async (formData) => {
     const response = await fetch(`${API_URL}/upload`, {
