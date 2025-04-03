9cbd29de88a5e634eb1ec1f6a94af836f9f80105
jainam03 <jbchheda03@gmail.com>
2025-04-03 12:51:23 +0530

    fix for routing issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    historyApiFallback: true,
+  },
   define: { 
     'import.meta.env.VITE_APP_API_URL': JSON.stringify(process.env.VITE_APP_API_URL), 
   },
